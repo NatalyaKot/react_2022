c7b67eacedc04603edf28035402c33c9c718f7df
Natalya <nkotkova92@gmail.com>
2022-02-26 16:16:06 +0300

    initial commit

---
+++ src/NameComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NameComponent = () => (
+  <div>
+    <p className="text">Natalya</p>
+    <p className="text">Kotkova</p>
+  </div>
+);
+
+export default NameComponent;
---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+// const element = <p className="text">Hello Word</p>;
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  // element,
+  document.getElementById('root')
+);
